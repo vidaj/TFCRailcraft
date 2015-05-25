a1f6cd90c2a9d1e90c9de41a1023dfdf1216b09a
Vidar Johansen <vidar@vidarjohansen.com>
2015-05-25 11:36:45 +0200

    Bumps version.

--- src/main/java/com/vidaj/tfcrailcraft/Constants.java
+++ src/main/java/com/vidaj/tfcrailcraft/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 	public static final String ModId = "tfcrailcraft";
 	
-	public static final String Version = "0.1";
+	public static final String Version = "0.1.1";
 	
 	public static final String Dependencies = "required-after:terrafirmacraft;required-after:Railcraft";
 }
